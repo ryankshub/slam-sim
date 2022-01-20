a818f47b47cebb3d561254fe6210e93ab63da8e6
Ryan King-Shepard <ryanking-shepard2022@u.northwestern.edu>
2022-01-19 20:34:57 -0600

    Implment function for Transform2D; can now get inv, translation, rotation

--- turtlelib/rigid2d.cpp
+++ turtlelib/rigid2d.cpp
@@ -1,6 +1,7 @@
 #include "rigid2d.hpp"
 #include <iostream>
 #include <string>
+#include <cmath>
 
 namespace turtlelib
 {
@@ -15,11 +16,13 @@ namespace turtlelib
     //over istream 
     std::istream & operator>>(std::istream & is, Vector2D & v)
     {
+        //Read from the input stream
         std::string x_str;
         std::string y_str;
         is >> x_str;
         is >> y_str;
 
+        //Check for bracket syntax
         if (x_str.front() == '[')
         {
             if (y_str.back() != ']')
@@ -32,11 +35,12 @@ namespace turtlelib
             }
         }
 
+        //Attempt to convert to a double.
         try 
         {
             double x = std::stod(x_str);
-            v.x = x;
             double y = std::stod(y_str);
+            v.x = x;
             v.y = y;
         } catch (const std::invalid_argument& e) {
             std::cout << e.what() << std::endl;
@@ -45,6 +49,88 @@ namespace turtlelib
         }
 
         return is;
+    }
+
+    //Transform2D Identity 
+    Transform2D::Transform2D() 
+        : mVec()
+        , mAng_rad(0)
+    {     
+    }
+
+    //Transform2D with only trans
+    Transform2D::Transform2D(Vector2D trans) 
+        : mVec(trans)
+        , mAng_rad(0)
+    {
+    }
+
+    //Transform2D only rotation
+    Transform2D::Transform2D(double radians)
+        : mVec()
+        , mAng_rad(radians)
+    {
+    }
+    
+    //Transform2 Full constrcutor
+    Transform2D::Transform2D(Vector2D trans, double radians)
+        : mVec(trans)
+        , mAng_rad(radians)
+    {
+    }
+
+    //Apply operation to vector
+    Vector2D Transform2D::operator()(Vector2D v) const
+    {
+        Vector2D new_vec;
+        new_vec.x = (v.x*std::cos(mAng_rad)) - (v.y*std::sin(mAng_rad)) + mVec.x;
+        new_vec.y = (v.x*std::sin(mAng_rad)) + (v.y*std::cos(mAng_rad)) + mVec.y;
+        return new_vec;
+    }
+
+    //Return inverse of transform
+    Transform2D Transform2D::inv() const
+    {
+        Vector2D vec;
+        vec.x = -mVec.x*std::cos(mAng_rad) - mVec.y*std::sin(mAng_rad);
+        vec.y = -mVec.y*std::cos(mAng_rad) + mVec.x*std::sin(mAng_rad);
+        double inv_ang = -mAng_rad;
+        Transform2D T{vec, inv_ang};
+        return T;
+    }
+
+    //Transform operator for matrix multiplication
+    Transform2D & Transform2D::operator*=(const Transform2D & rhs)
+    {
+        double new_ang = mAng_rad + rhs.mAng_rad;
+        double new_x = std::cos(mAng_rad)*rhs.mVec.x + std::sin(mAng_rad)*rhs.mVec.y + mVec.x;
+        double new_y = -std::sin(mAng_rad)*rhs.mVec.x + std::cos(mAng_rad)*rhs.mVec.y + mVec.y;
+
+        mAng_rad = new_ang;
+        mVec.x = new_x;
+        mVec.y = new_y; 
+
+        return *this;
+    }
+
+    //Get translation
+    Vector2D Transform2D::translation() const
+    {
+        return mVec;
+    }
+
+    //Get rotation
+    double Transform2D::rotation() const
+    {
+        return mAng_rad;
+    }
+
+    //Print function for translation
+    std::ostream & operator<<(std::ostream & os, const Transform2D & tf){
+        os << "deg: " << rad2deg(tf.mAng_rad);
+        os << " x: " << tf.mVec.x;
+        os << " y: " << tf.mVec.y;
+        return os;
     }
 
 }
--- turtlelib/rigid2d.hpp
+++ turtlelib/rigid2d.hpp
@@ -107,76 +107,79 @@ namespace turtlelib
     /// get removes the next unprocessed character from the buffer.
     std::istream & operator>>(std::istream & is, Vector2D & v);
 
-//     /// \brief a rigid body transformation in 2 dimensions
-//     class Transform2D
-//     {
-//     public:
-//         /// \brief Create an identity transformation
-//         Transform2D();
+    /// \brief a rigid body transformation in 2 dimensions
+    class Transform2D
+    {
+    public:
+        /// \brief Create an identity transformation
+        Transform2D();
 
-//         /// \brief create a transformation that is a pure translation
-//         /// \param trans - the vector by which to translate
-//         explicit Transform2D(Vector2D trans);
+        /// \brief create a transformation that is a pure translation
+        /// \param trans - the vector by which to translate
+        explicit Transform2D(Vector2D trans);
 
-//         /// \brief create a pure rotation
-//         /// \param radians - angle of the rotation, in radians
-//         explicit Transform2D(double radians);
+        /// \brief create a pure rotation
+        /// \param radians - angle of the rotation, in radians
+        explicit Transform2D(double radians);
 
-//         /// \brief Create a transformation with a translational and rotational
-//         /// component
-//         /// \param trans - the translation
-//         /// \param rot - the rotation, in radians
-//         Transform2D(Vector2D trans, double radians);
+        /// \brief Create a transformation with a translational and rotational
+        /// component
+        /// \param trans - the translation
+        /// \param rot - the rotation, in radians
+        Transform2D(Vector2D trans, double radians);
 
-//         /// \brief apply a transformation to a Vector2D
-//         /// \param v - the vector to transform
-//         /// \return a vector in the new coordinate system
-//         Vector2D operator()(Vector2D v) const;
-
-
-//         /// \brief invert the transformation
-//         /// \return the inverse transformation. 
-//         Transform2D inv() const;
-
-//         /// \brief compose this transform with another and store the result 
-//         /// in this object
-//         /// \param rhs - the first transform to apply
-//         /// \return a reference to the newly transformed operator
-//         Transform2D & operator*=(const Transform2D & rhs);
-
-//         /// \brief the translational component of the transform
-//         /// \return the x,y translation
-//         Vector2D translation() const;
-
-//         /// \brief get the angular displacement of the transform
-//         /// \return the angular displacement, in radians
-//         double rotation() const;
-
-//         /// \brief \see operator<<(...) (declared outside this class)
-//         /// for a description
-//         friend std::ostream & operator<<(std::ostream & os, const Transform2D & tf);
-
-//     };
+        /// \brief apply a transformation to a Vector2D
+        /// \param v - the vector to transform
+        /// \return a vector in the new coordinate system
+        Vector2D operator()(Vector2D v) const;
 
 
-//     /// \brief should print a human readable version of the transform:
-//     /// An example output:
-//     /// deg: 90 x: 3 y: 5
-//     /// \param os - an output stream
-//     /// \param tf - the transform to print
-//     std::ostream & operator<<(std::ostream & os, const Transform2D & tf);
+        /// \brief invert the transformation
+        /// \return the inverse transformation. 
+        Transform2D inv() const;
 
-//     /// \brief Read a transformation from stdin
-//     /// Should be able to read input either as output by operator<< or
-//     /// as 3 numbers (degrees, dx, dy) separated by spaces or newlines
-//     std::istream & operator>>(std::istream & is, Transform2D & tf);
+        /// \brief compose this transform with another and store the result 
+        /// in this object
+        /// \param rhs - the first transform to apply
+        /// \return a reference to the newly transformed operator
+        Transform2D & operator*=(const Transform2D & rhs);
 
-//     /// \brief multiply two transforms together, returning their composition
-//     /// \param lhs - the left hand operand
-//     /// \param rhs - the right hand operand
-//     /// \return the composition of the two transforms
-//     /// HINT: This function should be implemented in terms of *=
-//     Transform2D operator*(Transform2D lhs, const Transform2D & rhs);
+        /// \brief the translational component of the transform
+        /// \return the x,y translation
+        Vector2D translation() const;
+
+        /// \brief get the angular displacement of the transform
+        /// \return the angular displacement, in radians
+        double rotation() const;
+
+        /// \brief \see operator<<(...) (declared outside this class)
+        /// for a description
+        friend std::ostream & operator<<(std::ostream & os, const Transform2D & tf);
+    private:
+        Vector2D mVec;
+        double mAng_rad;
+        bool mInv;
+    };
+
+
+    /// \brief should print a human readable version of the transform:
+    /// An example output:
+    /// deg: 90 x: 3 y: 5
+    /// \param os - an output stream
+    /// \param tf - the transform to print
+    std::ostream & operator<<(std::ostream & os, const Transform2D & tf);
+
+    /// \brief Read a transformation from stdin
+    /// Should be able to read input either as output by operator<< or
+    /// as 3 numbers (degrees, dx, dy) separated by spaces or newlines
+    std::istream & operator>>(std::istream & is, Transform2D & tf);
+
+    /// \brief multiply two transforms together, returning their composition
+    /// \param lhs - the left hand operand
+    /// \param rhs - the right hand operand
+    /// \return the composition of the two transforms
+    /// HINT: This function should be implemented in terms of *=
+    Transform2D operator*(Transform2D lhs, const Transform2D & rhs);
 
 
 
