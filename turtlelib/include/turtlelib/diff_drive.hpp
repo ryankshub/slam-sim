545ceffe67481ac58d5253526131262d343bd73b
Ryan King-Shepard <ryanking-shepard2022@u.northwestern.edu>
2022-02-03 23:25:38 -0600

    Copied version of Diff Drive header file

---
+++ turtlelib/include/turtlelib/diff_drive.hpp
@@ -0,0 +1,73 @@
+#ifndef DIFF_DRIVE_INCLUDE_GUARD_HPP
+#define DIFF_DRIVE_INCLUDE_GUARD_HPP
+/// \file
+/// \brief Kinematics of a differential drive wheeled mobile robot
+
+
+namespace turtlelib {
+/// \brief Kinematics of 2-wheeled diff drive mobile robot
+    class DiffDrive
+    {
+    public:
+        /// \brief Create an identity transformation
+        Transform2D();
+
+        /// \brief create a transformation that is a pure translation
+        /// \param trans - the vector by which to translate
+        explicit Transform2D(Vector2D trans);
+
+        /// \brief create a pure rotation
+        /// \param radians - angle of the rotation, in radians
+        explicit Transform2D(double radians);
+
+        /// \brief Create a transformation with a translational and rotational
+        /// component
+        /// \param trans - the translation
+        /// \param rot - the rotation, in radians
+        Transform2D(Vector2D trans, double radians);
+
+        /// \brief apply a transformation to a Vector2D
+        /// \param v - the vector to transform
+        /// \return a vector in the new coordinate system
+        Vector2D operator()(Vector2D v) const;
+
+        /// \brief apply the adjunct to change the twist's frame
+        /// \param t - the twist to transform
+        /// \return a twist in the new coordinate system
+        Twist2D operator()(const Twist2D & t) const;
+
+        /// \brief invert the transformation
+        /// \return the inverse transformation. 
+        Transform2D inv() const;
+
+        /// \brief compose this transform with another and store the result 
+        /// in this object
+        /// \param rhs - the first transform to apply
+        /// \return a reference to the newly transformed operator
+        Transform2D & operator*=(const Transform2D & rhs);
+
+        /// \brief the translational component of the transform
+        /// \return the x,y translation
+        Vector2D translation() const;
+
+        /// \brief get the angular displacement of the transform
+        /// \return the angular displacement, in radians
+        double rotation() const;
+
+        /// \brief \see operator<<(...) (declared outside this class)
+        /// for a description
+        friend std::ostream & operator<<(std::ostream & os, const Transform2D & tf);
+    private:
+        Vector2D mVec;
+        double mAng_rad;
+    };
+
+    /// \brief should print a human readable version of the transform:
+    /// An example output:
+    /// deg: 90 x: 3 y: 5
+    /// \param os - an output stream
+    /// \param tf - the transform to print
+    std::ostream & operator<<(std::ostream & os, const Transform2D & tf);
+}
+
+#endif
